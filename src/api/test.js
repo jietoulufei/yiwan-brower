c3c8237a7ea5ad7e630889cf4c58c2bb677db47e
jietoulufei <914190601@qq.com>
2020-03-26 02:00:07 +0800

    22

--- src/api/test.js
+++ src/api/test.js
@@ -3,7 +3,7 @@ import request from "@/utils/request";
 // request.get("/db.json").then(response => {
 //   //vue-cli axios的默认路径为public/xxx
 //   const data = response.data;
-//   console.log(data);sss
+//   console.log(data);sss222
 // });
 
 export default {
