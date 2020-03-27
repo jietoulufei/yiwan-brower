2d0c55505aaeb2d1a6d900ad188ee3f0bf40cf2a
jietoulufei <914190601@qq.com>
2020-03-27 13:49:35 +0800

    移动端bug修复：禁止屏幕缩放,键盘遮挡

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,17 +6,14 @@
       :rules="rules"
       label-width="80px"
       class="login-from"
+      :style="{ margin: mediaMargin }"
     >
       <h2 class="login-title">壹萬小時</h2>
       <el-form-item label="账号" prop="username">
         <el-input v-model="form.username" placeholder="请输入账号"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input
-          v-model="form.password"
-          type="password"
-          placeholder="请输入密码"
-        ></el-input>
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit('form')">登录</el-button>
@@ -53,7 +50,9 @@ export default {
             trigger: "blur"
           }
         ]
-      }
+      },
+      mediaMargin: "160px auto 0px", //pc浏览器端样式
+      docmHeightInit: document.documentElement.clientHeight //浏览器屏幕初始高度
     };
   },
 
@@ -118,6 +117,28 @@ export default {
         }
       });
     }
+  },
+
+  mounted() {
+    console.log("navigator.userAgent", navigator.userAgent);
+    //当使用非PC端浏览器时候 监听屏幕高度
+    if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+      window.onresize = () => {
+        let { clientHeight } = document.documentElement;
+        clientHeight < this.docmHeightInit
+          ? (this.mediaMargin = "0px auto")
+          : (this.mediaMargin = "160px auto 0px");
+      };
+
+      //meta标签的user-scalable=0只能禁止双击缩放，所以此处需要禁止两手指缩放页面
+      document.documentElement.addEventListener(
+        "touchmove",
+        function(event) {
+          if (event.touches.length > 1) event.preventDefault();
+        },
+        false
+      );
+    }
   }
 };
 </script>
@@ -127,6 +148,19 @@ export default {
 @imgURL: "https://ae01.alicdn.com/kf/H385e95e46be04983a270d5e96e1e82feM.jpg";
 // 登录框label颜色
 @color: rgb(66, 58, 58);
+
+//媒体查询样式
+.mediaWidth(@width_size:350px) {
+  width: @width_size;
+}
+
+.mediaLogin {
+  // 上左右下间隙，左右自动居中 当浏览器高度变太小时，会出现纵向滚动条，原因是margin-top:160px
+  // margin: 160px auto 0px;
+  background-color: rgba(89, 128, 159, 0.5);
+  padding: 28px;
+  border-radius: 10px;
+}
 
 .login-container {
   position: absolute;
@@ -146,13 +180,20 @@ export default {
     text-align: center;
   }
 
-  .login-from {
-    width: 350px;
-    // 上左右下间隙，左右自动居中 当浏览器高度变太小时，会出现纵向滚动条，原因是margin-top:160px
-    margin: 160px auto 0px;
-    background-color: rgba(89, 128, 159, 0.5);
-    padding: 28px;
-    border-radius: 10px;
+  // 如果文档宽度小于 260 像素则修改
+  @media only screen and (max-width: 400px) {
+    .login-from {
+      .mediaWidth(260px);
+      .mediaLogin();
+    }
+  }
+
+  // 如果文档宽度大于 401 像素则修改
+  @media only screen and (min-width: 401px) {
+    .login-from {
+      .mediaWidth();
+      .mediaLogin();
+    }
   }
 
   .el-form-item__label {
