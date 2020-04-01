4a59b0d73abfc16f610375a0dcf86eeb6ae4bdfc
jietoulufei <914190601@qq.com>
2020-04-02 00:22:51 +0800

    添加路由过渡效果

--- src/components/AppMain/index.vue
+++ src/components/AppMain/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="main">
     <!-- 注意不要把组件取名<link>因为是内置标签 -->
+    <!-- 可以通过 appear attribute 设置节点在初始渲染的过渡 -->
     <AppLink></AppLink>
-    <router-view></router-view>
+    <transition :name="transitionName" appear>
+      <router-view class="Router"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -10,7 +13,9 @@
 import AppLink from "./link";
 export default {
   data() {
-    return {};
+    return {
+      transitionName: "fade"
+    };
   },
 
   components: { AppLink },
@@ -18,3 +23,28 @@ export default {
   methods: {}
 };
 </script>
+
+<style lang="less">
+.Router {
+  position: absolute; //正常情况下过度动画会占位，隐藏的老页面会占位，新页面进来会往上移，所以需absolute
+  left: 0;
+  right: 0;
+}
+//老页面移除隐藏过渡
+.fade-leave-active {
+  transition: all 1s;
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+//新页面移入显示过渡
+.fade-enter-active {
+  transition: all 1.5s;
+}
+.fade-enter {
+  opacity: 0;
+  transform: translateX(-200%);
+}
+</style>
