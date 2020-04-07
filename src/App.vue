e0d97d9bbed32a20614b4a02708d1a0bc2d5ddfc
jietoulufei <914190601@qq.com>
2020-04-07 23:45:39 +0800

    新增首屏loading功能

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
   </div>
 </template>
 
+<script>
+export default {
+  created: function() {
+    //当vue实例创建时，去除首屏loading
+    document.body.removeChild(document.getElementById("loader-wrapper"));
+  }
+};
+</script>
+
 <style lang="less">
 body {
   font-family: "微软雅黑";
